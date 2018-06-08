f004b719164d7b89c9223af313506ef19236516f
wenndy <wenddy@gmail.com>
2018-06-08 14:31:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/preweather/com/preweather/db/Province.java
@@ -0,0 +1,42 @@
+package android.preweather.com.preweather.db;
+
+
+import android.os.Bundle;
+import android.preweather.com.preweather.R;
+import org.litepal.crud.DataSupport;
+/**
+ * Created by bauhinia on 2018/6/7.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(int provinceCode) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+
+}
